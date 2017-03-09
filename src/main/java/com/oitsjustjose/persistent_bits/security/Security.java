2d989df9853cbae9c4ec88aa39958c0b580ada81
oitsjustjose <stovallj1995@gmail.com>
2017-03-09 09:53:29 -0500

    Re-working security if possible...

--- src/main/java/com/oitsjustjose/persistent_bits/security/Security.java
+++ src/main/java/com/oitsjustjose/persistent_bits/security/Security.java
@@ -11,13 +11,15 @@ import com.oitsjustjose.persistent_bits.tileentity.TileChunkLoader;
 
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class Security
 {
 	@SubscribeEvent
-	public void registerEvent(BreakSpeed event)
+	public void registerEvent(PlayerInteractEvent event)
 	{
 		World world = event.getEntity().getEntityWorld();
 		GameProfile harvester = new GameProfile(event.getEntityPlayer().getUniqueID(), event.getEntityPlayer().getName());
@@ -32,7 +34,17 @@ public class Security
 				GameProfile owner = tileChunkLoader.getOwner();
 				if (!(owner.getId().equals(harvester.getId())))
 				{
-					event.setNewSpeed(0);
+					if (event.isCancelable())
+						event.setCanceled(true);
+					event.setResult(Result.DENY);
+					return;
+				}
+				if (event.getEntityPlayer() instanceof FakePlayer)
+				{
+					if (event.isCancelable())
+						event.setCanceled(true);
+					event.setResult(Result.DENY);
+					return;
 				}
 			}
 		}
